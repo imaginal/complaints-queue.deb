459ab3f2245a468395f17b1d0fa152ea01e73637
Volodymyr Flonts <flyonts@gmail.com>
2016-07-10 06:06:56 +0300

    initial commit v0.2-2

---
+++ srv/complaints.queue/bin/complaints_angel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BASEDIR=/srv/complaints.queue
+PIDFILE=/run/complaints_angel.pid
+INIFILE=/etc/complaints.queue/complaints_queue.ini
+LOGFILE=/var/log/complaints.queue/complaints_queue.log
+PIDCHLD=`grep pidfile $INIFILE | cut -d= -f2`
+
+on_exit()
+{
+  test -s $PIDCHLD && kill `cat $PIDCHLD`
+  test -f $PIDFILE && rm -f $PIDFILE
+  test -f nohup.out && rm nohup.out
+  exit
+}
+
+echo $$ >$PIDFILE
+trap on_exit SIGINT SIGTERM
+
+while true
+do
+  $BASEDIR/bin/complaints_queue $INIFILE >>$LOGFILE 2>&1
+  sleep 10
+done
